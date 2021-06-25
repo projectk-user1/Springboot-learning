bd2330fe5b69fc5dd91c27a057080e875d9d9679
Mahesh <mahesh.bongiri@ge.com>
2021-06-25 15:57:24 +0530

    getting-started added

---
+++ getting_started/src/main/java/com/maheshb/getting_started/GettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.maheshb.getting_started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedApplication.class, args);
+	}
+
+}
---
+++ getting_started/src/test/java/com/maheshb/getting_started/GettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maheshb.getting_started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
